f65b6e312dce21db1c9b74226956dca5dc82ff82
Jared Johnson <jared@jaredjohnson.dev>
2022-06-28 21:35:11 -0400

    Setup test container

--- test/main.py
+++ test/main.py
@@ -1,1 +1,20 @@
-print('hello world')
+import unittest
+from selenium import webdriver
+
+class WebdriverTests(unittest.TestCase):
+
+    def setUp(self):
+      options = webdriver.ChromeOptions()
+      options.add_argument('--headless')
+      options.add_argument('--no-sandbox')
+      options.add_argument('--disable-gpu')
+      options.add_argument('--disable-setuid-sandbox')
+      self.browser = webdriver.Remote('http://chrome:4444/wd/hub', options=options)
+      self.addCleanup(self.browser.quit)
+
+    def testPageTitle(self):
+        self.browser.get('http://app:8000')
+        self.assertIn('foo', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
